5f2f3996751057d53e7388aa6b65284dd649e3d3
Stullet <lirabusiness7@gmail.com>
2025-11-18 01:35:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7188873350-1d929",
+  "appId": "1:948957650538:web:dc22fb4d53fbbce7bb3413",
+  "apiKey": "AIzaSyApLiyfUvogrq88FuR0aOjL81fRy0bT1bg",
+  "authDomain": "studio-7188873350-1d929.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "948957650538"
+};
